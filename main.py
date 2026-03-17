7ea0ad8925d62c6c3f1a6a9313e069acdf946dc9
JoseOrdonezB <ord231329@uvg.edu.gt>
2026-03-16 19:37:51 -0600

    main para probar la minimizacion

--- main.py
+++ main.py
@@ -1,61 +1,29 @@
 from src.regex import preparar_regex
 from src.afd_directo import construir_afd_directo
-from src.transiciones import imprimir_tabla_transiciones
-from src.simulacion import simular_afd, verificar_cadena
+from src.minimizacion import minimizar_afd
 
 regex = input("Ingrese la expresión regular: ")
 
 regex_preparada, alfabeto = preparar_regex(regex)
 
-print("\nRegex preparada:", regex_preparada)
-print("Alfabeto:", alfabeto)
-
 afd = construir_afd_directo(regex_preparada, alfabeto)
 
-print("\nPOSTFIJO:", afd["postfijo"])
+afd_min = minimizar_afd(afd, alfabeto)
 
-print("\nFOLLOWPOS")
-for pos in sorted(afd["followpos"]):
-    print(pos, "->", sorted(afd["followpos"][pos]))
+estados_directo = len(afd["estados"])
+trans_directo = sum(len(v) for v in afd["transiciones"].values())
 
-print("\nESTADOS")
-for estado in afd["estados"]:
-    nombre = afd["nombres"][estado]
-    print(nombre, "=", set(estado))
+estados_min = len(afd_min["estados"])
+trans_min = sum(len(v) for v in afd_min["transiciones"].values())
 
-print("\nESTADO INICIAL")
-print(afd["nombres"][afd["estado_inicial"]])
+# expresión regular para probar: (a|b)*abb(a|b)*
 
-print("\nESTADOS DE ACEPTACION")
-for estado in afd["aceptacion"]:
-    print(afd["nombres"][estado], "=", set(estado))
+print("RESULTADO DE MINIMIZACION")
 
-print("\nTRANSICIONES")
-for estado, trans in afd["transiciones"].items():
+print("\nAFD DIRECTO")
+print("Estados:", estados_directo)
+print("Transiciones:", trans_directo)
 
-    nombre = afd["nombres"][estado]
-
-    for simbolo, destino in trans.items():
-        print(nombre, "--", simbolo, "-->", afd["nombres"][destino])
-
-imprimir_tabla_transiciones(afd, alfabeto)
-
-# Verificación de cadenas
-while True:
-
-    cadena = input("\nIngrese una cadena (o escriba 'salir'): ")
-
-    if cadena.lower() == "salir":
-        print("Finalizando programa.")
-        break
-
-    pertenece = verificar_cadena(afd, cadena, alfabeto)
-
-    if pertenece:
-        print("\nResultado final: CADENA ACEPTADA ✅")
-    else:
-        print("\nResultado final: CADENA RECHAZADA ❌")
-
-    # Recorrido detallado
-    print("\n--- Detalle de la simulación ---")
-    simular_afd(afd, cadena, alfabeto)
+print("\nAFD MINIMIZADO")
+print("Estados:", estados_min)
+print("Transiciones:", trans_min)
